f7e7e97be580d849d79deec4a573b24942b12477
TiinaJuk <gustiiju@student.gu.se>
2017-05-08 12:02:57 +0300

    Update host.java
    The newest working code for server

--- JavaWiFiService/host.java
+++ JavaWiFiService/host.java
@@ -11,77 +11,82 @@ import java.util.logging.Logger;
 import com.fazecast.jSerialComm.*;
 import arduino.*;
 
-class host
-{
-    public static void main(String srgs[])
-    {
-        ServerSocket serverSocket = null;
-        Socket socket = null;
+class host2test {
+	static Arduino arduino = null;
+	public static void main(String args[]) {
 
-        BufferedReader bufferedReader = null;
-        PrintStream printStream = null;
-        Arduino arduino;
+		ServerSocket serverSocket = null;
+		Socket socket = null;
 
+		BufferedReader bufferedReader = null;
+		PrintStream printStream = null;
+		//Arduino arduino = null;
+		PrintWriter printWriter = null;
+		SerialPort serialPort = SerialPort.getCommPort("/dev/ttyACM0");
+		try {
+			serverSocket = new ServerSocket(1337);
 
-        try{
-            serverSocket = new ServerSocket(1337);
+			System.out.println(serverSocket.getInetAddress() + ", " + serverSocket.toString());
+			System.out.println("I'm waiting here: " + serverSocket.getLocalPort());
 
-            socket = serverSocket.accept();
+			socket = serverSocket.accept();
+			System.out.println("from " + socket.getInetAddress() + ":" + socket.getPort());
 
-            System.out.println(serverSocket.getInetAddress() + ", " + serverSocket.toString());
-            System.out.println("I'm waiting here: "
-                    + serverSocket.getLocalPort());
+			InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+			bufferedReader = new BufferedReader(inputStreamReader);
 
-            socket = serverSocket.accept();
-            System.out.println("from " +
-                    socket.getInetAddress() + ":" + socket.getPort());
+			OutputStream outputStream = socket.getOutputStream();
+			printWriter = new PrintWriter(outputStream, true);
 
-            InputStreamReader inputStreamReader =
-                    new InputStreamReader(socket.getInputStream());
-            bufferedReader = new BufferedReader(inputStreamReader);
+			arduino = new Arduino("/dev/ttyACM0", 9600);
+			arduino.openConnection();
+			
+			Thread t1 = new Thread(new Runnable() {
+				public void run() {
+					PrintWriter printWriter = new PrintWriter(outputStream, true);
+					String serialRead;
+					while (true) {
+						serialRead = arduino.serialRead(1);
+						printWriter.println(serialRead);
+						System.out.println(serialRead);
+						try{
+							Thread.sleep(100);
+						}catch (InterruptedException e) {
+							System.out.print(e.toString());
+						}
+					}
+				}
+			});
+			t1.start();
+			
+			String serialRead;
+			while (bufferedReader != null) {
+				String command = bufferedReader.readLine();
+				System.out.println(command);
+				arduino.serialWrite(command);
+			}
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					System.out.print(ex.toString());
+				}
+			}
 
-            OutputStream outputStream = socket.getOutputStream();
-            printWriter = new PrintWriter(outputStream, true);
+			if (printStream != null) {
+				printStream.close();
+			}
 
-            arduino = new Arduino("/dev/ttyACM0", 9600);
-            arduino.openConnection();
-
-
-            while (bufferedReader != null)
-            {
-                String serialRead = arduino.readSerial();
-                arduino.serialWrite(bufferedReader.readLine());
-                printWriter.println(serialRead);
-            }
-            
-            /**
-            String line;
-            while((line=bufferedReader.readLine()) != null){
-                System.out.println(line);
-            }
-            */
-        }catch(IOException e){
-            System.out.println(e.toString());
-        }finally{
-            if(bufferedReader!=null){
-                try {
-                    bufferedReader.close();
-                } catch (IOException ex) {
-                    System.out.print(ex.toString());
-                }
-            }
-
-            if(printStream!=null){
-                printStream.close();
-            }
-
-            if(socket!=null){
-                try {
-                    socket.close();
-                } catch (IOException ex) {
-                    System.out.print(ex.toString());
-                }
-            }
-        }
-    }
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException ex) {
+					System.out.print(ex.toString());
+				}
+			}
+		}
+	}
 }
